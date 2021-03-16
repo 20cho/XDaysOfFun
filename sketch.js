39f8a9229371b5dce75dd0b6d3f7f556d397098c
Marin <mcgermain.024@gmail.com>
2021-03-16 21:05:09 +0100

    J'ai fait un joli dessin maîtresse

--- sketch.js
+++ sketch.js
@@ -1,7 +1,6 @@
-let x = 0;
-let y = 0;
-let spacing = 10; 
-
+let x = 0; //coordonnées 
+let y = 0 ;
+let diameter = 10; 
 
 
 function setup() {
@@ -10,15 +9,35 @@ function setup() {
 }
 
 function draw() {
-  stroke(255);
+  strokeWeight (4);
+  stroke(0);
+  fill (242,60,19);
   if (random (1) < 0.5) {
-    line(x, y, x + spacing, y + spacing);
+    circle(x + diameter, y, diameter);
   }else{
-    line(x, y + spacing, x + spacing, y);  
+    strokeWeight (0.5);
+    stroke(0);
+    fill (242,89,75); 
+    circle(x, y, diameter);
   }
-  x = x + spacing
+
+  if (random (1) < 0.2) {
+    strokeWeight (6);
+    stroke(0);
+    fill (255,255,0);
+    circle(x + diameter, y, diameter);
+  }
+
+
+
+  if (random (1)< 0.5) {
+    x = x + diameter;
+  }else{
+    x = x + diameter + y/2 ;
+  }
+
   if (x > width) { 
     x = 0;
-    y = y + spacing;
+    y = y + diameter;
   }  
 }
